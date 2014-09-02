6aa3da7816f440b569406150458c4a40cc529a0e
JH-WTHS <hardyj@worc.k12.ma.us>
2014-09-02 14:48:27 -0400

    The correct beginning

---
+++ test_source.java
@@ -0,0 +1,9 @@
+/* Test source code for AP CS Worcester Technical High School */
+
+public class test_source{
+
+   public static void main (String args[]){
+      System.out.println("This is a test");
+      
+   }
+   }
